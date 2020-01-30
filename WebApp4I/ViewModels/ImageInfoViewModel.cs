c24a3f3d1ec455ababd97d0ad04bc3cea1f38d04
Vitalij Sorokin <sv89@tut.by>
2020-01-30 21:14:11 +0300

    Добавлено недостающее свойство к модели представления

--- WebApp4I/ViewModels/ImageInfoViewModel.cs
+++ WebApp4I/ViewModels/ImageInfoViewModel.cs
@@ -4,6 +4,7 @@ namespace WebApp4I.ViewModels
 {
     public class ImageInfoViewModel
     {
+        public int Id { get; set; }
         public string Path { get; set; }
         public string Description { get; set; }
         public IDictionary<string, string> Metadata { get; set; }
