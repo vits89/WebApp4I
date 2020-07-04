9e6fa81c3f2d6a4abd2b66097af7c282b22992a5
Vitalij Sorokin <sv89@tut.by>
2020-07-04 23:27:34 +0300

    Добавлена сборка и минификация CSS- и JS-файлов

---
+++ WebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace WebApp4I.WebApp
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include("~/Scripts/jquery-{version}.slim.js",
+                "~/Scripts/bootstrap.bundle.js",
+                "~/Scripts/vue.js",
+                "~/Scripts/vee-validate.full.js"));
+        }
+    }
+}
--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Http;
+using System.Web.Optimization;
 
 namespace WebApp4I.WebApp
 {
@@ -14,6 +15,7 @@ namespace WebApp4I.WebApp
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             AutoMapperConfig.Initialize();
         }
